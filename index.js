055439d3c3149e96b3a68e22710998b36d70b9e1
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-05 13:32:48 +0300

    Added server with Hello World page on endpoint /

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// TODO: Start prototyping here
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+class MyServer {
+
+    listen () {
+        return new Promise((resolve, reject) => {
+            this.server = app.listen(3000, () => resolve()).on('error', reject);
+            console.log('Listening on port 3000');
+        });
+    }
+
+    close () {
+        return new Promise((resolve, reject) => {
+            this.server = this.server.close(() => resolve()).on('error', reject);
+        });
+    }
+}
+
+if (require.main === module) {
+    const server = new MyServer();
+
+    server.listen();
+}
+
+exports.MyServer = MyServer;
---
+++ test/index-test.js
@@ -0,0 +1,33 @@
+const app = require('../index.js');
+const { expect } = require('chai');
+const got = require('got');
+
+describe('Server', () => {
+    const myServer = new app.MyServer();
+
+    before(() => {
+        myServer.listen();
+    });
+
+    after(() => {
+        myServer.close();
+    });
+
+    it(`Sends response from endpoint '/'`, async () => {
+        const expectedStatus = 200;
+
+        const response = await got('http://localhost:3000');
+
+        expect(response.statusCode).equal(expectedStatus);
+    });
+
+    it(`Sends the correct html`, async () => {
+        const expectedBody = 'Hello Node.js';
+        const bodyRegex = /<body.*?>([\s\S]*)<\/body>/;
+
+        const response = await got('http://localhost:3000');
+        const actualBody = bodyRegex.exec(response.body)[1].trim();
+
+        expect(actualBody).equal(expectedBody);
+    });
+});
