c425cd0c67936bd47a63892f8278b849c1d7dac2
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-05 16:08:24 +0300

    Using debug instead of console.log

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const config = require('./config.js').config();
+const path = require('path');
+const debug = require('debug')('server');
 
 const app = express();
 
@@ -12,8 +14,8 @@ class MyServer {
 
     listen (port = config.port, host = config.host) {
         return new Promise((resolve, reject) => {
-            this.server = app.listen(port, host, () => resolve()).on('error', reject);
-            console.log(`Listening on port ${port}`);
+            this.server = app.listen(portNum, () => resolve()).on('error', reject);
+            debug(`Listening on port ${portNum}`);
         });
     }
 
