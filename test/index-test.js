7ed56ea951d949a117279ba3aab8598cb0a3807e
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-06 18:57:14 +0300

    Now provided .env is considered a default configuration + added tests on that

--- test/index-test.js
+++ test/index-test.js
@@ -2,19 +2,40 @@ const path = require('path');
 const app = require('../src/index.js');
 const { expect } = require('chai');
 const got = require('got');
+const configImport = require('../src/config.js');
 
 describe('Server', () => {
     const myServer = new app.MyServer();
-    const host = 'localhost';
-    const port = '3000';
-    const rootPath = path.join(`http://${host}:${port}`);
+    const config = configImport.config();
+    const defaults = configImport.defaults;
+
+    const rootPath = path.join(`http://${config.host}:${config.port}`);
 
     before(() => {
-        myServer.listen(port, host);
+        myServer.listen(config.port, config.host);
     });
 
     after(() => {
         myServer.close();
+    });
+
+    it('Listens on default host and port if no .env configuration file is provided', () => {
+        process.env.PORT = '';
+        process.env.HOST = '';
+
+        const defaultConfig = configImport.config();
+
+        expect(defaultConfig).eql(defaults);
+    });
+
+    it('Listens on host and port provided in a .env file', () => {
+        process.env.PORT = 'nondefault';
+        process.env.HOST = 'nondefault';
+
+        const nonDefaultConfig = configImport.config();
+        const expectedConfig = { port: process.env.PORT, host: process.env.HOST };
+
+        expect(nonDefaultConfig).eql(expectedConfig);
     });
 
     it(`Sends response from endpoint '/'`, async () => {
