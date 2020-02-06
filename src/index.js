d98580e8c9084da634e4ebde397f07e9d0193e46
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-06 15:09:05 +0300

    Default host/port are now provided via config/.env

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const app = express();
 const path = require('path');
+const express = require('express');
 
-const staticPath = path.join(__dirname, '..', 'resources', 'html');
+require('dotenv').config({ path: path.join(__dirname, '../config/.env') });
+const app = express();
+
+const staticPath = path.join(__dirname, '../resources/html');
 
 app.use(express.static(staticPath));
 
 class MyServer {
 
-    listen () {
-        const portNum = '3000';
-
+    listen (port = process.env.PORT, host = process.env.HOST) {
         return new Promise((resolve, reject) => {
-            this.server = app.listen(portNum, () => resolve()).on('error', reject);
-            console.log(`Listening on port ${portNum}`);
+            this.server = app.listen(port, host, () => resolve()).on('error', reject);
+            console.log(`Listening on port ${port}`);
         });
     }
 
--- test/index-test.js
+++ test/index-test.js
@@ -1,12 +1,16 @@
+const path = require('path');
 const app = require('../src/index.js');
 const { expect } = require('chai');
 const got = require('got');
 
+require('dotenv').config({ path: path.join(__dirname, '../config/.env') });
+
 describe('Server', () => {
     const myServer = new app.MyServer();
+    const rootPath = path.join(`http://${process.env.TEST_HOST}:${process.env.TEST_PORT}`);
 
     before(() => {
-        myServer.listen();
+        myServer.listen(process.env.TEST_PORT, process.env.TEST_HOST);
     });
 
     after(() => {
@@ -16,7 +20,7 @@ describe('Server', () => {
     it(`Sends response from endpoint '/'`, async () => {
         const expectedStatus = 200;
 
-        const response = await got('http://localhost:3000');
+        const response = await got(rootPath);
 
         expect(response.statusCode).equal(expectedStatus);
     });
@@ -25,7 +29,7 @@ describe('Server', () => {
         const expectedBody = 'Hello Node.js';
         const bodyRegex = /<body.*?>([\s\S]*)<\/body>/;
 
-        const response = await got('http://localhost:3000');
+        const response = await got(rootPath);
         const actualBody = bodyRegex.exec(response.body)[1].trim();
 
         expect(actualBody).equal(expectedBody);
