4599f0cf2912d7f0bc768a703d73bd33a823fd0b
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-13 16:29:28 +0300

    Moved constants (and temporarily - access key) to config and .env

--- src/Server.js
+++ src/Server.js
@@ -19,25 +19,22 @@ class Server {
         this.app.use(express.static(this.staticPath));
 
         this.app.get('/users', async (req, res) => {
-            const baseURL = 'https://graph.microsoft.com/beta';
-            const token = 'eyJ0eXAiOiJKV1QiLCJub25jZSI6IlR2VE5WV0pnQjVDZERLOUxkV0YxR0U2ZFJiZXFXb0swdlFKS3BVU1R5VXMiLCJhbGciOiJSUzI1NiIsIng1dCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83NzU3NDNhMS03ZGEyLTQ3ODUtODMzZS1hOGVjYmYzN2M5MzcvIiwiaWF0IjoxNTgxNTk1ODUxLCJuYmYiOjE1ODE1OTU4NTEsImV4cCI6MTU4MTU5OTc1MSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IjQyTmdZRkJRMGFtZTBDSDRlTXJsSzhLdkRwajBQWitVdGFuR2c5ZXJ2ZkJUN0s1YnY1a0EiLCJhbXIiOlsicHdkIl0sImFwcF9kaXNwbGF5bmFtZSI6IlRlYW0gTGluayIsImFwcGlkIjoiMzdiNjA0ZWItZGJiOC00OTVjLTk2MzgtODg0YjNiOWM1ZDcyIiwiYXBwaWRhY3IiOiIxIiwiZmFtaWx5X25hbWUiOiJBZG1pbiIsImdpdmVuX25hbWUiOiIyMTAiLCJpcGFkZHIiOiI4Ni4xMTAuMTk1LjEwNiIsIm5hbWUiOiIyMTAgQWRtaW4iLCJvaWQiOiJiY2NmZjY0Yi03N2ZjLTQwNGEtYTViOC1mMjA4YzdhMGYxYjYiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDA5QUMwMzg1NiIsInNjcCI6IkNoYXQuUmVhZFdyaXRlIEdyb3VwLlJlYWQuQWxsIE1haWwuUmVhZCBQcmVzZW5jZS5SZWFkLkFsbCBVc2VyLlJlYWQgVXNlci5SZWFkLkFsbCBwcm9maWxlIG9wZW5pZCBlbWFpbCIsInN1YiI6Ii1VdXE4LWhPMWpVbk8teXE2RzNtNTBQWlNnRm9NSm41U0VHWExKR0QwQUkiLCJ0aWQiOiI3NzU3NDNhMS03ZGEyLTQ3ODUtODMzZS1hOGVjYmYzN2M5MzciLCJ1bmlxdWVfbmFtZSI6IjIxMF9hZG1pbkBkeGRldmVsb3Blci5vbm1pY3Jvc29mdC5jb20iLCJ1cG4iOiIyMTBfYWRtaW5AZHhkZXZlbG9wZXIub25taWNyb3NvZnQuY29tIiwidXRpIjoiR1F5MWREMWdwRWk0ZnY5OFhHTHVBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiNjkwOTEyNDYtMjBlOC00YTU2LWFhNGQtMDY2MDc1YjJhN2E4Il0sInhtc19zdCI6eyJzdWIiOiJyZlBBNURqdS1SNWpCTFpHVmZiQW9RRC1tR3kxQWRObF82Q2RMWlJTbXowIn0sInhtc190Y2R0IjoxNTgxNDA5MjEyfQ.RXm6t0rORF7aPHKIW-KOD3SFfD9tj9WTPnYKC9m126ciLQ29gQq3ENuW6BjcrtV2Kio23BXnYjB2AcscBNkDqbo97nWCyKVz9uWoXhwyAOwW2t_hT5FTss6CeyPr5Gpwtt8rXgjl-0Rn_1jPqCDEDJOaXq_5wws2G1SjGIP5yq-iamTse3cmlZgOnrna8Bct0r-DZieYCxpyYaarD1aGAeb7apygjQbcCp87SgQ1do8hewIVlKN621XaPdwYiC-_yF06BjnqarcsdT7Qzmc8Xt04uU9MC7qlI8PCvYT5afYXIjjDtoWixEJAuJah6TEU1nYhXsxIZUe5jC8cZUta3w';
-
             const groupQuery = `/groups?$filter=startswith(displayName,'dxdeveloper')&$select=displayName,id`;
-            const groupURL = path.join(baseURL, groupQuery);
+            const groupURL = path.join(config.apiBaseURL, groupQuery);
             
-            const groupRes = await got(groupURL, {headers: {Authorization: `Bearer ${token}`}});
+            const groupRes = await got(groupURL, {headers: {Authorization: `${config.accessType} ${config.accessToken}`}});
             const group = JSON.parse(groupRes.body).value[0];
 
             const channelQuery = `/teams/${group.id}/channels?$filter=startswith(displayName, 'General')&select=displayName,id`;
-            const channelURL = path.join(baseURL, channelQuery);
+            const channelURL = path.join(config.apiBaseURL, channelQuery);
 
-            const channelRes = await got(channelURL, {headers: {Authorization: `Bearer ${token}`}});
+            const channelRes = await got(channelURL, {headers: {Authorization: `${config.accessType} ${config.accessToken}`}});
             const channel = JSON.parse(channelRes.body).value[0];
 
             const usersQuery = `/teams/${group.id}/channels/${channel.id}/members`;
-            const usersURL = path.join(baseURL, usersQuery);
+            const usersURL = path.join(config.apiBaseURL, usersQuery);
 
-            const usersRes = await got(usersURL, {headers: {Authorization: `Bearer ${token}`}});
+            const usersRes = await got(usersURL, {headers: {Authorization: `${config.accessType} ${config.accessToken}`}});
             const users = JSON.parse(usersRes.body).value.map((data) => {
                 return {
                     displayName: data.displayName,
@@ -48,9 +45,9 @@ class Server {
             const onlineUsers = []; 
             for (let user of users) {
                 const userStatusQuery = `/users/${user.id}/presence`;
-                const userStatusURL = path.join(baseURL, userStatusQuery);
+                const userStatusURL = path.join(config.apiBaseURL, userStatusQuery);
 
-                const userStatusRes = await got(userStatusURL, {headers: {Authorization: `Bearer ${token}`}});
+                const userStatusRes = await got(userStatusURL, {headers: {Authorization: `${config.accessType} ${config.accessToken}`}});
                 const userStatus = {
                     displayName: user.displayName,
                     id: user.id,
--- src/config.js
+++ src/config.js
@@ -2,7 +2,10 @@ const dotenv = require('dotenv');
 
 const port = '8080';
 const host = 'localhost';
-const staticPath = '../resources/html'
+const staticPath = '../resources/html';
+const accessType = 'Bearer';
+const accessToken = 'eyJ0eXAiOiJKV1QiLCJub25jZSI6IndNVHZDYUp3aXY1NG44a1hDWW14d245bVhtZ1UzM0x5VlNhUFV0LVBOY0UiLCJhbGciOiJSUzI1NiIsIng1dCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83NzU3NDNhMS03ZGEyLTQ3ODUtODMzZS1hOGVjYmYzN2M5MzcvIiwiaWF0IjoxNTgxNDI2NzQzLCJuYmYiOjE1ODE0MjY3NDMsImV4cCI6MTU4MTQzMDY0MywiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IjQyTmdZTkQ0WnZKMFcrZjJnMHdTdHNKdlBTMzNIOWxnZXpLaWY5TXRweDdOOThJU3VtVUEiLCJhbXIiOlsicHdkIl0sImFwcF9kaXNwbGF5bmFtZSI6IlRlYW0gTGluayIsImFwcGlkIjoiMzdiNjA0ZWItZGJiOC00OTVjLTk2MzgtODg0YjNiOWM1ZDcyIiwiYXBwaWRhY3IiOiIxIiwiZmFtaWx5X25hbWUiOiJBZG1pbiIsImdpdmVuX25hbWUiOiIyMTAiLCJpcGFkZHIiOiI4Ni4xMTAuMTk1LjEwNiIsIm5hbWUiOiIyMTAgQWRtaW4iLCJvaWQiOiJiY2NmZjY0Yi03N2ZjLTQwNGEtYTViOC1mMjA4YzdhMGYxYjYiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDA5QUMwMzg1NiIsInNjcCI6IkNoYXQuUmVhZFdyaXRlIE1haWwuUmVhZCBVc2VyLlJlYWQgcHJvZmlsZSBvcGVuaWQgZW1haWwiLCJzdWIiOiItVXVxOC1oTzFqVW5PLXlxNkczbTUwUFpTZ0ZvTUpuNVNFR1hMSkdEMEFJIiwidGlkIjoiNzc1NzQzYTEtN2RhMi00Nzg1LTgzM2UtYThlY2JmMzdjOTM3IiwidW5pcXVlX25hbWUiOiIyMTBfYWRtaW5AZHhkZXZlbG9wZXIub25taWNyb3NvZnQuY29tIiwidXBuIjoiMjEwX2FkbWluQGR4ZGV2ZWxvcGVyLm9ubWljcm9zb2Z0LmNvbSIsInV0aSI6IkJRWjhleGdUSGtpLVNNTnlGYkdEQUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjY5MDkxMjQ2LTIwZTgtNGE1Ni1hYTRkLTA2NjA3NWIyYTdhOCJdLCJ4bXNfc3QiOnsic3ViIjoicmZQQTVEanUtUjVqQkxaR1ZmYkFvUUQtbUd5MUFkTmxfNkNkTFpSU216MCJ9LCJ4bXNfdGNkdCI6MTU4MTQwOTIxMn0.Am2DkE-h-3WLB1xs9wXLJM1xUMom98f6Mn8B7sG2AKbHO6uxXTSG2SVcmVYH_gyvhUY-XrFsFiXezX7b-EMPfDJFzOg7t2qXVOW2kAystm7mtjKUn906jvhdHz0AWZkkrMBFhwMHOyzWFY0d7zJjrOurgMhhPLK5rYdL2HLjZT3J2o4ZBKahhaiQZlM2zSPrWGm4qyzKMkr0zBw7bKJVcUqEEF2s8EPLjyvYBtEm3dxmtHSp381PEen7Dld9SCK5lyeqJyU4OsuYogvN5CSOuP9SrNjZIye4T15V19UUY8DImQUU8m2Yaq0IndMn7J9mU_fnfhWek-dTDV7RU53HkQ';
+const apiBaseURL = 'https://graph.microsoft.com/beta';
 
 dotenv.config();
 
@@ -10,7 +13,10 @@ const config = () => {
     return {
         port: process.env.PORT? process.env.PORT : port,
         host: process.env.HOST? process.env.HOST : host,
-        staticPath: process.env.STATIC_PATH? process.env.STATIC_PATH : staticPath
+        staticPath: process.env.STATIC_PATH? process.env.STATIC_PATH : staticPath,
+        accessType: process.env.OAUTH_ACCESS_TYPE? process.env.OAUTH_ACCESS_TYPE : accessType,
+        accessToken: process.env.OAUTH_ACCESS_TOKEN_USER? process.env.OAUTH_ACCESS_TOKEN_USER : accessToken,
+        apiBaseURL: process.env.API_BASE_URL? process.env.API_BASE_URL : apiBaseURL
     }
 }
 
@@ -19,6 +25,9 @@ module.exports = {
     defaults: {
         port, 
         host, 
-        staticPath
+        staticPath,
+        accessType,
+        accessToken,
+        apiBaseURL
     }
 }
