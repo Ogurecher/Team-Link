d2b56bc87b6e548e5e8493793f0d5161b41b68a6
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-13 13:08:38 +0300

    Now properly returning all available channel members

--- src/Server.js
+++ src/Server.js
@@ -19,20 +19,60 @@ class Server {
 
         this.app.get('/users', async () => {
             console.log('users');
-            const baseURL = 'https://graph.microsoft.com/beta/';
-            const usersURL = path.join(baseURL, 'users');
-            const groupsQuery = `/groups?$filter=startswith(displayName,'dxdeveloper')&$select=displayName,id`;
-            const token = 'eyJ0eXAiOiJKV1QiLCJub25jZSI6IkVRYUlGWnMyRGhuMkFhOGk1TmRfckNFeDFTTnE2T0JmZHlUbjJnSUJWMDgiLCJhbGciOiJSUzI1NiIsIng1dCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83NzU3NDNhMS03ZGEyLTQ3ODUtODMzZS1hOGVjYmYzN2M5MzcvIiwiaWF0IjoxNTgxNDM1MTk2LCJuYmYiOjE1ODE0MzUxOTYsImV4cCI6MTU4MTQzOTA5NiwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IjQyTmdZSEJqRitoakRvbWNmTXY0clR2M2RrV1pXOU5iZjZ5ZXd6ZG5TbUxWcWVCV3QxZ0EiLCJhbXIiOlsicHdkIl0sImFwcF9kaXNwbGF5bmFtZSI6IlRlYW0gTGluayIsImFwcGlkIjoiMzdiNjA0ZWItZGJiOC00OTVjLTk2MzgtODg0YjNiOWM1ZDcyIiwiYXBwaWRhY3IiOiIxIiwiZmFtaWx5X25hbWUiOiJBZG1pbiIsImdpdmVuX25hbWUiOiIyMTAiLCJpcGFkZHIiOiI4Ni4xMTAuMTk1LjEwMCIsIm5hbWUiOiIyMTAgQWRtaW4iLCJvaWQiOiJiY2NmZjY0Yi03N2ZjLTQwNGEtYTViOC1mMjA4YzdhMGYxYjYiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDA5QUMwMzg1NiIsInNjcCI6IkNoYXQuUmVhZFdyaXRlIE1haWwuUmVhZCBQcmVzZW5jZS5SZWFkLkFsbCBVc2VyLlJlYWQgVXNlci5SZWFkLkFsbCBwcm9maWxlIG9wZW5pZCBlbWFpbCIsInN1YiI6Ii1VdXE4LWhPMWpVbk8teXE2RzNtNTBQWlNnRm9NSm41U0VHWExKR0QwQUkiLCJ0aWQiOiI3NzU3NDNhMS03ZGEyLTQ3ODUtODMzZS1hOGVjYmYzN2M5MzciLCJ1bmlxdWVfbmFtZSI6IjIxMF9hZG1pbkBkeGRldmVsb3Blci5vbm1pY3Jvc29mdC5jb20iLCJ1cG4iOiIyMTBfYWRtaW5AZHhkZXZlbG9wZXIub25taWNyb3NvZnQuY29tIiwidXRpIjoiR29zMjduNFU0RW1mdEZEZkhFYU1BQSIsInZlciI6IjEuMCIsIndpZHMiOlsiNjkwOTEyNDYtMjBlOC00YTU2LWFhNGQtMDY2MDc1YjJhN2E4Il0sInhtc19zdCI6eyJzdWIiOiJyZlBBNURqdS1SNWpCTFpHVmZiQW9RRC1tR3kxQWRObF82Q2RMWlJTbXowIn0sInhtc190Y2R0IjoxNTgxNDA5MjEyfQ.qOYPpClwDItOzUZjeAmrm0WGhEMVadEf-6KPnCqPU4Ta5vjiw5GygAFqrfe1rDYNyT08pMwnHy2rbbADejbsdhNVgEkDiq2bLEBuaMwmw12nwl1LDUTnxk6DsZC6w3Hrx2Uhq9t4bHEYqx0EBz3-eWrIS5jC7porVm3CoHBMIfBwm9W6w_aH5FZryWw72bv83xzsx_gsD9kPgU5l6uULsyWTq4I9v9IFxpKVEsZYL49FzDLP-wHLanHwyhZvB9L7RhCzyCV-lHuWVn3RJGL65ZwiS2Pb-g2fIPjKKaFaFKneKG08bzYTrFcgF0wUnNYaoEGQtMFKkX2MhLhRmR2bPg';
-            const result = await got(usersURL, {headers: {Authorization: `Bearer ${token}`}});
-            const users = JSON.parse(result.body).value.map(({id, displayName}) => {
-                return { id, displayName };
+            const baseURL = 'https://graph.microsoft.com/beta';
+            const token = 'eyJ0eXAiOiJKV1QiLCJub25jZSI6InMtb1pqRl8yS0NnSzB6WVlXRkxWVmNHLWxyQ0pLTlpGQ1dQWk5RRkJxa1EiLCJhbGciOiJSUzI1NiIsIng1dCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSIsImtpZCI6IkhsQzBSMTJza3hOWjFXUXdtak9GXzZ0X3RERSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83NzU3NDNhMS03ZGEyLTQ3ODUtODMzZS1hOGVjYmYzN2M5MzcvIiwiaWF0IjoxNTgxNTg1MDY4LCJuYmYiOjE1ODE1ODUwNjgsImV4cCI6MTU4MTU4ODk2OCwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFTUUEyLzhPQUFBQThHSVZiTTVDd0dUaTQ2SGw1bFZRN3Z1NWw0YzEzd1hhRTJJRFduRkl0cUk9IiwiYW1yIjpbInB3ZCJdLCJhcHBfZGlzcGxheW5hbWUiOiJUZWFtIExpbmsiLCJhcHBpZCI6IjM3YjYwNGViLWRiYjgtNDk1Yy05NjM4LTg4NGIzYjljNWQ3MiIsImFwcGlkYWNyIjoiMSIsImZhbWlseV9uYW1lIjoiQWRtaW4iLCJnaXZlbl9uYW1lIjoiMjEwIiwiaXBhZGRyIjoiODYuMTEwLjE5NS4xMDAiLCJuYW1lIjoiMjEwIEFkbWluIiwib2lkIjoiYmNjZmY2NGItNzdmYy00MDRhLWE1YjgtZjIwOGM3YTBmMWI2IiwicGxhdGYiOiIzIiwicHVpZCI6IjEwMDMyMDAwOUFDMDM4NTYiLCJzY3AiOiJDaGF0LlJlYWRXcml0ZSBHcm91cC5SZWFkLkFsbCBNYWlsLlJlYWQgUHJlc2VuY2UuUmVhZC5BbGwgVXNlci5SZWFkIFVzZXIuUmVhZC5BbGwgcHJvZmlsZSBvcGVuaWQgZW1haWwiLCJzdWIiOiItVXVxOC1oTzFqVW5PLXlxNkczbTUwUFpTZ0ZvTUpuNVNFR1hMSkdEMEFJIiwidGlkIjoiNzc1NzQzYTEtN2RhMi00Nzg1LTgzM2UtYThlY2JmMzdjOTM3IiwidW5pcXVlX25hbWUiOiIyMTBfYWRtaW5AZHhkZXZlbG9wZXIub25taWNyb3NvZnQuY29tIiwidXBuIjoiMjEwX2FkbWluQGR4ZGV2ZWxvcGVyLm9ubWljcm9zb2Z0LmNvbSIsInV0aSI6IkVudnJpWHMtTGtpcUZ5bWRSeDN5QUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbIjY5MDkxMjQ2LTIwZTgtNGE1Ni1hYTRkLTA2NjA3NWIyYTdhOCJdLCJ4bXNfc3QiOnsic3ViIjoicmZQQTVEanUtUjVqQkxaR1ZmYkFvUUQtbUd5MUFkTmxfNkNkTFpSU216MCJ9LCJ4bXNfdGNkdCI6MTU4MTQwOTIxMn0.nlswZy9M1qt6oUsUTxjzMnunu5SGtAVm3UA7UVPE6EAaMW6RMPZTLHEjtpdhgR9Ook-cK413A6bVcOzwMLf4RvUEJv1RMe_W5J-9ul22EMI5mlBAY0ELwVHFmMSmuhtgeexh6VNpMQ0eJb1VZxk8B2JyRF6UiMNbR-5m6xyj12T7BZ5wFLMFax0ZxiSgDvBKENNbNgPdKkdzUmAOzhS-DZV_2gnKN8fLuDn7-qfsH-Fp0cqXehy2Qm5qpi7NkgrkMTv1kavo6pDhKKPRZ1NguYO6Q47KHY3oTeDfYF3KWW9QmeAhnV0WV_I7pNPK0ROZuuxbkBpoLJOX7jpO8QaQdQ';
+
+            const groupQuery = `/groups?$filter=startswith(displayName,'dxdeveloper')&$select=displayName,id`;
+            const groupURL = path.join(baseURL, groupQuery);
+            
+            const group = await got(groupURL, {headers: {Authorization: `Bearer ${token}`}})
+            .then((res) => {
+                return JSON.parse(res.body).value[0];
             });
+            console.log(group);
+
+            const channelQuery = `/teams/${group.id}/channels?$filter=startswith(displayName, 'General')&select=displayName,id`;
+            const channelURL = path.join(baseURL, channelQuery);
+
+            const channel = await got(channelURL, {headers: {Authorization: `Bearer ${token}`}})
+            .then((res) => {
+                return JSON.parse(res.body).value[0];
+            });
+            console.log(channel);
+
+            const usersQuery = `/teams/${group.id}/channels/${channel.id}/members`;
+            const usersURL = path.join(baseURL, usersQuery);
+
+            const users = await got(usersURL, {headers: {Authorization: `Bearer ${token}`}})
+            .then((res) => {
+                return JSON.parse(res.body).value.map((data) => {
+                    return {
+                        displayName: data.displayName,
+                        id: data.userId
+                    }
+                });
+            });
+            console.log(users);
+            
+            const onlineUsers = []; 
             for (let user of users) {
-                const userOnlineURL = path.join(usersURL, user.id, '/presence');
-                const res = await got(userOnlineURL, {headers: {Authorization: `Bearer ${token}`}});
-                console.log(user.displayName);
-                console.log(JSON.parse(res.body).availability);
-            }
+                const userStatusQuery = `/users/${user.id}/presence`;
+                const userStatusURL = path.join(baseURL, userStatusQuery);
+
+                const userStatus = await got(userStatusURL, {headers: {Authorization: `Bearer ${token}`}})
+                .then((res) => {
+                    return {
+                        displayName: user.displayName,
+                        status: JSON.parse(res.body).availability
+                    }
+                });
+
+                if (userStatus.status === 'Available') {
+                    onlineUsers.push(userStatus);
+                };
+            };
+
+            console.log(onlineUsers);
         });
     }
 
