694386f5b24b844c1981086bc7fa0feab07d4d29
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-14 15:15:21 +0300

    Properly exporting attachCORSHeaders as a function

--- src/routes/headers.js
+++ src/routes/headers.js
@@ -1,9 +1,11 @@
-module.exports = {
-    attachCORSHeaders: function ({ res }) {
-        res.header('Access-Control-Allow-Origin', ['*']);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
+function attachCORSHeaders ({ res }) {
+    res.header('Access-Control-Allow-Origin', ['*']);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-        return res;
-    }
+    return res;
+}
+
+module.exports = {
+    attachCORSHeaders
 };
