38c7b2eb3bd14beee5bd40e473088ab248fcfad7
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-21 10:28:14 +0300

    Added handlers for unhandledRejection and uncaughtException

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 import App from './App';
+import debug from 'debug';
+
+const error = debug('team-link:error');
+
+process.on('unhandledRejection', () => {
+    error('Unhandled promise rejection');
+    throw new Error('Unhandled promise rejection');
+});
+
+process.on('uncaughtException', () => {
+    error('Uncaught exception');
+    throw new Error('Uncaught exception');
+});
 
 if (process.mainModule === module)
     App.create();
