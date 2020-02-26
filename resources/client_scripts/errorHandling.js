c4abc2954b5c771ff8deec7dd694adb3cbb7d563
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-26 10:55:53 +0300

    Using debug module in errorHandling.js

--- resources/client_scripts/errorHandling.js
+++ resources/client_scripts/errorHandling.js
@@ -1,9 +1,15 @@
+const debug = window.debug;
+localStorage.debug = 'team-link:*';
+
+const info = debug('team-link:info');
+const error = debug('team-link:error');
+
 window.onerror = (err) => {
-    console.log(`An error occured. More info: ${err}`); // TODO: replace console.log() with debug
+    error(`An error occured. More info: ${err}`);
     throw err;
 };
 
 window.onunhandledrejection = (err) => {
-    console.log(`Unhandled rejection. More info: ${err}`);
+    error(`Unhandled rejection. More info: ${err}`);
     throw err;
 };
