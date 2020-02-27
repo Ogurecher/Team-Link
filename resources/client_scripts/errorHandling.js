40d6ebed96cdcd7575103eda568e072977797fb4
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-02-27 11:57:00 +0300

    Sending error messages from client to server

--- resources/client_scripts/errorHandling.js
+++ resources/client_scripts/errorHandling.js
@@ -4,12 +4,29 @@ localStorage.debug = 'team-link:*';
 const info = debug('team-link:info');
 const error = debug('team-link:error');
 
+const getErrorInfo = (err) => {
+    return {
+        type: err.type,
+        stack: err.reason.stack
+    }
+}
+
+const sendError = (err) => {
+    fetch('http://localhost:3000/clientError', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(getErrorInfo(err))
+    });
+}
+
 window.onerror = (err) => {
     error(`An error occured. More info: ${err}`);
-    throw err;
+    sendError(err);
 };
 
 window.onunhandledrejection = (err) => {
     error(`Unhandled rejection. More info: ${err}`);
-    throw err;
+    sendError(err);
 };
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { getOnlineUsers } from './users';
+import { handleClientError } from './clientError';
 
 export const router = express.Router();
 
 router.route('/users').get(getOnlineUsers);
+router.route('/clientError').post(handleClientError);
