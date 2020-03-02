f9cd716b5661f39f8c9c2e2e2c322788885486fc
Dmitry Peredreev (DevExpress) <peredreev.dmitry@corp.devexpress.com>
2020-03-02 10:35:46 +0300

    Removed leftover eslint rule disabler

--- src/client_scripts/clientConfig.ts
+++ src/client_scripts/clientConfig.ts
@@ -1,9 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-unused-vars */
-
 export default {
     pollingInterval: 5000
 };
-
-/* eslint-enable @typescript-eslint/no-unused-vars */
-/* eslint-enable no-unused-vars */
