530c50a4b0acb7d0b20dabf80e67190c1d4b8b49
Dmitrii <1@1.1>
2020-04-24 16:28:51 +0300

    Trying to pass notifications over from node server. Unauthorized

--- src/routes/callback.ts
+++ src/routes/callback.ts
@@ -1,3 +1,4 @@
+import got from 'got';
 import debugModule from 'debug';
 import events from 'events';
 import { CallbackRequest, HTTPResponse } from '../interfaces';
@@ -16,6 +17,10 @@ export async function callback (req: CallbackRequest, res: HTTPResponse): Promis
         debug('call terminated');
         notifier.emit('Call terminated');
     }
+
+    got.post('https://myhuebot.ngrok.io/api/calls', {
+        json: req.body
+    });
 
     res.status(200);
     res.end();
