e2a8d7fe594441914e4fef96a10f3fa5a85c4552
Dmitrii <1@1.1>
2020-05-15 13:36:33 +0300

    Refactored toggle functions

--- src/client_scripts/toggleAudio.ts
+++ src/client_scripts/toggleAudio.ts
@@ -1,17 +1,5 @@
-import { pc } from './setUpMedia';
+import { toggleMedia } from './toggleMedia';
 
 export async function toggleAudio (): Promise<void> {
-    const senders: RTCRtpSender[] = pc.getSenders();
-
-    if (senders[0] !== null) {
-        const audioTrack = senders[0].track;
-
-        if (audioTrack !== null) {
-            if (audioTrack.enabled === false)
-                audioTrack.enabled = true;
-            else
-                audioTrack.enabled = false;
-
-        }
-    }
+    toggleMedia('audio');
 }
--- src/client_scripts/toggleVideo.ts
+++ src/client_scripts/toggleVideo.ts
@@ -1,17 +1,5 @@
-import { pc } from './setUpMedia';
+import { toggleMedia } from './toggleMedia';
 
 export async function toggleVideo (): Promise<void> {
-    const senders: RTCRtpSender[] = pc.getSenders();
-
-    if (senders[1] !== null) {
-        const videoTrack = senders[1].track;
-
-        if (videoTrack !== null) {
-            if (videoTrack.enabled === false)
-                videoTrack.enabled = true;
-            else
-                videoTrack.enabled = false;
-
-        }
-    }
+    toggleMedia('video');
 }
